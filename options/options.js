e0c723f987902e68fc21fbcc4dd7c1ca8886a75b
Crowdbotics <team@crowdbotics.com>
2023-04-24 02:14:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "glow-bracelets-40152",
+  url: "https://glow-bracelets-40152.botics.co",
+  api: "https://glow-bracelets-40152.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
